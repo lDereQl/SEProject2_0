c06e411c853ffe51b361f55c62e7149b4674a4d1
Артем Грошев <artemgrosev@MacBook-Air-Artem.local>
2024-03-08 21:24:07 +0200

    Reg./Log. screens complete

--- src/view/components/input.js
+++ src/view/components/input.js
@@ -2,26 +2,29 @@ import React from 'react';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 
-const Input = ({label, iconName, password}) => {
+const Input = ({label, iconName, error, password, ... props}) => {
     const [hidePassword, setHidePassword] = React.useState(password);
 
     return(
         <View style = {style.container}>
             <Text>{label}</Text>
-            <View style = {style.input}>
+            <View style = {[style.input,]}>
                 <Icon name = {iconName} style = {style.icon}></Icon>
-                <TextInput style = {style.textInput} 
-                secureTextEntry = {hidePassword}/>
-                {
-                    password && (
-                        <Icon
-                          onPress = {() => setHidePassword(!hidePassword)}
-                          name = {hidePassword ? "eye" : "eye-slash"} 
-                          style = {style.iconEye}
-                         />
-                    )
+                <TextInput 
+                    style = {style.textInput} 
+                    secureTextEntry = {hidePassword}
+                    {... props}
+                />
+                {password && (
+                     <Icon
+                     onPress = {() => setHidePassword(!hidePassword)}
+                     name = {hidePassword ? "eye" : "eye-slash"} 
+                     style = {style.iconEye}
+                     />
+                     )
                 }
             </View>
+            {error && <Text style = {style.textError}> {error} </Text>}
         </View>
     )
 }
@@ -37,6 +40,8 @@ const style = StyleSheet.create({
         paddingHorizontal: 15,
         borderWidth: 0.75,
         alignItems: "center",
+        borderRadius: 200,
+        borderWidth: 1,
     },
     icon:{
         fontSize: 25,
@@ -51,6 +56,11 @@ const style = StyleSheet.create({
         flex: 1,
         marginLeft: 10,
     },
+    textError: {
+        color: "red",
+        fontSize: 12,
+        marginTop: 7,
+    }
 });
 
 export default Input;
--- src/view/screens/LoginScreen.js
+++ src/view/screens/LoginScreen.js
@@ -1,11 +1,173 @@
-import { View, Text } from 'react-native';
+import React from "react";
+import { StyleSheet, SafeAreaView, View, Text, ScrollView, Image } from 'react-native';
+import { ALERT_TYPE, Dialog, AlertNotificationRoot, Toast } from 'react-native-alert-notification';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const LoginScreen = () => {
+import Button from '../components/button';
+import Input from '../components/input';
+import Loader from '../components/loader';
+import Logo from '../../images/travel.png';
+
+const LoginScreen = ({navigation}) => {
+
+  const [input, setInputs] = React.useState({
+    email: "",
+    password: "",
+  })
+
+  const [errors, setErrors] = React.useState({})
+  const [loading, setLoader] = React.useState(false)
+
+  const validate = async () => {
+    console.log("validate");
+    console.log(input);
+    let isValid = true;
+
+    if(!input.email){
+        handleError("Please enter the email", "email")
+        isValid = false
+    }else if(!input.email.match(/\S+@\S+\.\S+/)){
+        handleError("Please enter the valid email", "email")
+        isValid = false
+    }
+    if(!input.password){
+        handleError("Please enter the password", "password")
+        isValid = false
+    }else if (input.password.length < 8){
+        handleError("Minimal password length is 8", "password")
+        isValid = false
+    }
+
+    if (isValid) login();
+  };
+
+  const handleError = (text, input) => {
+    setErrors((prevState) => ({ ...prevState, [input] : text}));
+  };
+
+  const handleOnChange = (text, input) => {
+      setInputs((prevState) => ({ ...prevState, [input] : text}));
+  };
+  
+  const login = () => {
+    console.log("login!");
+    console.log(input) ;
+    
+    setLoader(true);
+    setTimeout(async() => {
+        try {
+            setLoader(false);
+            let userData = await AsyncStorage.getItem("userData");
+
+            if(userData){
+              userData = JSON.parse(userData);
+              console.log("userData");
+              console.log(userData);
+
+              if(input.email == userData.email &&
+                input.password == userData.password)
+              {
+                navigation.navigate("HomeScreen");
+                AsyncStorage.setItem(
+                  "userData",
+                  JSON.stringify({...userData, loggedIn: true})
+                )
+              }
+              else 
+              {
+                console.log("No such account");
+                Dialog.show({
+                  type: ALERT_TYPE.DANGER, 
+                  title: "Error",
+                  textBody: "Incorrect email/password!",
+                  button: "close"
+                }); 
+              }
+            }else {
+              console.log("No such account");
+              Dialog.show({
+                type: ALERT_TYPE.DANGER, 
+                title: "Error",
+                textBody: "No account found!",
+                button: "close"
+              });
+            }
+        } catch (error) {
+          console.log("Error");
+            Dialog.show({
+                type: ALERT_TYPE.DANGER, 
+                title: "Error",
+                textBody: error,
+                button: "close"
+            });
+        }
+    }, 1000);
+  };
+
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Login Screen</Text>
-      </View>
+      <AlertNotificationRoot style={style.container}>
+        <SafeAreaView style={style.container}>
+        <Loader visible = {loading}/>
+          <ScrollView style = {style.svContainer}>
+            <Image style = {style.image} source = {Logo}></Image>
+            <Text style = {style.textTitle}>
+                Login Form
+            </Text>
+            <View style = {style.viewContainer}>
+            <Input 
+                iconName = 'envelope'
+                placeholder = "Enter your Email"
+                onChangeText = {(text) => handleOnChange(text, "email")}
+                onFocus = {() => handleError(null, "email")}
+                error = {errors.email}
+            />
+            <Input 
+                iconName ='key'  
+                password placeholder = "Enter your Password"
+                onChangeText = {(text) => handleOnChange(text, "password")}
+                onFocus = {() => handleError(null, "password")}
+                error = {errors.password}
+            />
+            </View>
+            <Button title = "Login" onPress = {validate}/>
+            <Text style = {style.signin} onPress = {() => 
+              navigation.navigate("RegistrScreen")}>Sign in</Text>
+          </ScrollView>
+        </SafeAreaView>
+      </AlertNotificationRoot>
     );
   }
+
+  const style = StyleSheet.create({
+    container: { 
+      flex: 1, 
+      backgroundColor: "white",
+    },
+    svContainer: {
+      paddingTop: 20, 
+      paddingHorizontal: 20,
+    },
+    viewContainer: {
+      paddingTop: 20,
+    },
+    textTitle:{
+      paddingTop: 20, 
+      fontSize: 20,
+      fontWeight: "bold",
+      color: "black",
+      alignSelf: "center",
+    },
+    image:{
+      width: 250,
+      height: 250,
+      alignSelf: "center",
+    },
+    signin: {
+      textAlign: "center",
+      fontSize: 16,
+      color: "black",
+      fontWeight: "bold",
+    },
+  });
 
 export default LoginScreen;
