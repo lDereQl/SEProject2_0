7d4d51c57303cac592d809f3ebb29d940ec2bdfd
Артем Грошев <artemgrosev@MacBook-Air-Artem.local>
2024-05-02 13:01:24 +0300

    TRP-2 Preparing the template

--- components/Home/BestHotels.jsx
+++ components/Home/BestHotels.jsx
@@ -1,16 +1,16 @@
-import { View, TouchableOpacity, StyleSheet } from 'react-native'
+import { StyleSheet, TouchableOpacity, View } from 'react-native'
 import React from 'react'
-import reusable from '../Reusable/reusable.style';
-import { TEXT, COLORS } from '../../constants/theme';
-import ReusableText from '../Reusable/ReusableText';
-import Feather from '@expo/vector-icons'
+import { TEXT, COLORS } from '../../constants/theme'
+import reusable from '../Reusable/reusable.style'
+import ReusableText from '../Reusable/ReusableText'
+import { Feather } from '@expo/vector-icons'
 
 const BestHotels = () => {
   return (
     <View>
       <View style={[reusable.rowWithSpace('space-between'), {paddingBottom: 20}]}>
         <ReusableText
-              text={'Recomendations'}
+              text={'Nearby Hotels'}
               family={"medium"}
               size={TEXT.large}
               color={COLORS.black}
